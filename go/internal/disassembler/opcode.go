ed266b583c60a02a9e739e24a733cb5d61bb19f5
Jacob Deal <jacob.r.deal@gmail.com>
2019-09-12 02:47:36 -0400

    clean up code and add remaining opcodes disassembler instructions

--- go/internal/disassembler/opcode.go
+++ go/internal/disassembler/opcode.go
@@ -16,28 +16,106 @@ func (o Opcode) Bytes() (firstByte, secondByte byte) {
 	return byte(o >> 8), byte(o)
 }
 
-// Disassemble returns the Opcode's name and instruction. If the Opcode is
+// Instruction returns the Opcode's name and instruction. If the Opcode is
 // unknown then ErrUnknownOp will be returned.
-func (o Opcode) Disassemble() (name, instruction string, err error) {
+func (o Opcode) Instruction() (string, error) {
 	firstByte, secondByte := o.Bytes()
 
-	switch firstByte >> 4 {
-	case 0x06:
-		name = "MVI"
-		instruction = fmt.Sprintf(
-			"V%01X,#$%02x",
-			firstByte&0xF,
-			secondByte,
-		)
-	case 0x0A:
-		name = "MVI"
-		instruction = fmt.Sprintf(
-			"I,#$%01x%02x",
-			firstByte&0x0F,
-			secondByte,
-		)
+	firstNib := firstByte >> 4
+	secondNib := firstByte & 0xf
+	thirdNib := secondByte >> 4
+	fourthNib := secondByte & 0xf
+
+	switch firstNib {
+	case 0x0:
+		switch secondByte {
+		case 0xe0:
+			return fmt.Sprintf("%-10s", "CLS"), nil
+		case 0xee:
+			return fmt.Sprintf("%-10s", "RTS"), nil
+		default:
+			return "", ErrUnknownOp
+		}
+	case 0x1:
+		return fmt.Sprintf("%-10s $%01x%02X", "JUMP", secondNib, secondByte), nil
+	case 0x2:
+		return fmt.Sprintf("%-10s $%01x%02X", "CALL", secondNib, secondByte), nil
+	case 0x3:
+		return fmt.Sprintf("%-10s V%01X,#$%02X", "SKIP.EQ", secondNib, secondByte), nil
+	case 0x4:
+		return fmt.Sprintf("%-10s V%01X,#$%02X", "SKIP.NE", secondNib, secondByte), nil
+	case 0x5:
+		return fmt.Sprintf("%-10s V%01X,V%01X", "SKIP.EQ", secondNib, thirdNib), nil
+	case 0x6:
+		return fmt.Sprintf("%-10s V%01X,#$%02X", "MVI", secondNib, secondByte), nil
+	case 0x7:
+		return fmt.Sprintf("%-10s V%01X,#$%02X", "ADI", secondNib, secondByte), nil
+	case 0x8:
+		switch secondByte & 0xf {
+		case 0x0:
+			return fmt.Sprintf("%-10s V%01X,V%01X", "MOV", secondNib, thirdNib), nil
+		case 0x1:
+			return fmt.Sprintf("%-10s V%01X,V%01X", "OR", secondNib, thirdNib), nil
+		case 0x2:
+			return fmt.Sprintf("%-10s V%01X,V%01X", "AND", secondNib, thirdNib), nil
+		case 0x3:
+			return fmt.Sprintf("%-10s V%01X,V%01X", "XOR", secondNib, thirdNib), nil
+		case 0x4:
+			return fmt.Sprintf("%-10s V%01X,V%01X", "ADD.", secondNib, thirdNib), nil
+		case 0x5:
+			return fmt.Sprintf("%-10s V%01X,V%01X", "SUB.", secondNib, thirdNib), nil
+		case 0x6:
+			return fmt.Sprintf("%-10s V%01X", "SHR.", secondNib), nil
+		case 0x7:
+			return fmt.Sprintf("%-10s V%01X,V%01X", "SUBB.", secondNib, thirdNib), nil
+		case 0xe:
+			return fmt.Sprintf("%-10s V%01X", "SHL.", secondNib), nil
+		default:
+			return "", ErrUnknownOp
+		}
+	case 0x9:
+		return fmt.Sprintf("%-10s V%01X,V%01X", "SKIP.NE", secondNib, thirdNib), nil
+	case 0xa:
+		return fmt.Sprintf("%-10s I,#$%01x%02X", "MVI", secondNib, secondByte), nil
+	case 0xb:
+		return fmt.Sprintf("%-10s $%01x%02X(V0)", "JUMP", secondNib, secondByte), nil
+	case 0xc:
+		return fmt.Sprintf("%-10s V%01X,#$%02X", "RND", secondNib, secondByte), nil
+	case 0xd:
+		return fmt.Sprintf("%-10s V%01X,V%01X,#$%01X", "SPRITE.", secondNib, thirdNib, fourthNib), nil
+	case 0xe:
+		switch secondByte {
+		case 0x9E:
+			return fmt.Sprintf("%-10s V%01X", "SKIP.KEY", secondNib), nil
+		case 0xA1:
+			return fmt.Sprintf("%-10s V%01X", "SKIP.NOKEY", secondNib), nil
+		default:
+			return "", ErrUnknownOp
+		}
+	case 0xf:
+		switch secondByte {
+		case 0x07:
+			return fmt.Sprintf("%-10s V%01X,DELAY", "MOV", secondNib), nil
+		case 0x0a:
+			return fmt.Sprintf("%-10s V%01X", "WAITKEY", secondNib), nil
+		case 0x15:
+			return fmt.Sprintf("%-10s DELAY,V%01X", "MOV", secondNib), nil
+		case 0x18:
+			return fmt.Sprintf("%-10s SOUND,V%01X", "MOV", secondNib), nil
+		case 0x1e:
+			return fmt.Sprintf("%-10s I,V%01X", "ADD", secondNib), nil
+		case 0x29:
+			return fmt.Sprintf("%-10s V%01X", "SPRITECHAR", secondNib), nil
+		case 0x33:
+			return fmt.Sprintf("%-10s V%01X", "MOVBCD", secondNib), nil
+		case 0x55:
+			return fmt.Sprintf("%-10s (I),V0-V%01X", "MOVM", secondNib), nil
+		case 0x65:
+			return fmt.Sprintf("%-10s V0-V%01X,(I)", "MOVM", secondNib), nil
+		default:
+			return "", ErrUnknownOp
+		}
 	default:
-		err = ErrUnknownOp
+		return "", ErrUnknownOp
 	}
-	return
 }
--- go/internal/disassembler/opcode_test.go
+++ go/internal/disassembler/opcode_test.go
@@ -13,37 +13,218 @@ func TestOpcode_Disassemble(t *testing.T) {
 	type testCase struct {
 		label               string
 		opcode              disassembler.Opcode
-		expectedName        string
 		expectedInstruction string
 	}
 	cases := []testCase{
 		{
-			label:               "6xkk",
-			opcode:              0x6208,
-			expectedName:        "MVI",
-			expectedInstruction: "V2,#$08",
+			label:               "00E0 clear the display",
+			opcode:              0x00E0,
+			expectedInstruction: "CLS       ",
 		},
 		{
-			label:               "Annn",
+			label:               "00EE return from a subroutine",
+			opcode:              0x00EE,
+			expectedInstruction: "RTS       ",
+		},
+		{
+			label:               "1nnn jump to location nnn",
+			opcode:              0x128A,
+			expectedInstruction: "JUMP       $28A",
+		},
+		{
+			label:               "2nnn call subroutine at nnn",
+			opcode:              0x228A,
+			expectedInstruction: "CALL       $28A",
+		},
+		{
+			label:               "3xkk skip next instruction if Vx == kk",
+			opcode:              0x3A00,
+			expectedInstruction: "SKIP.EQ    VA,#$00",
+		},
+		{
+			label:               "4xkk skip next instruction if Vx != kk",
+			opcode:              0x4800,
+			expectedInstruction: "SKIP.NE    V8,#$00",
+		},
+		{
+			label:               "5xy0 skip next instruction if Vx == Vy",
+			opcode:              0x5A70,
+			expectedInstruction: "SKIP.EQ    VA,V7",
+		},
+		{
+			label:               "6xkk set Vx = kk",
+			opcode:              0x6208,
+			expectedInstruction: "MVI        V2,#$08",
+		},
+		{
+			label:               "7xkk set Vx = Vx + kk",
+			opcode:              0x7B93,
+			expectedInstruction: "ADI        VB,#$93",
+		},
+		{
+			label:               "8xy0 set Vx = Vy",
+			opcode:              0x83A0,
+			expectedInstruction: "MOV        V3,VA",
+		},
+		{
+			label:               "8xy1 set Vx = Vx OR Vy",
+			opcode:              0x83A1,
+			expectedInstruction: "OR         V3,VA",
+		},
+		{
+			label:               "8xy2 set Vx = Vx AND Vy",
+			opcode:              0x83A2,
+			expectedInstruction: "AND        V3,VA",
+		},
+		{
+			label:               "8xy3 set Vx = Vx XOR Vy",
+			opcode:              0x83A3,
+			expectedInstruction: "XOR        V3,VA",
+		},
+		{
+			label:               "8xy4 set Vx = Vx + Vy, set VF = carry",
+			opcode:              0x83B4,
+			expectedInstruction: "ADD.       V3,VB",
+		},
+		{
+			label:               "8xy5 set Vx = Vx - Vy, set VF = NOT borrow",
+			opcode:              0x83B5,
+			expectedInstruction: "SUB.       V3,VB",
+		},
+		{
+			label:               "8xy6 set Vx = Vx SHR 1",
+			opcode:              0x83B6,
+			expectedInstruction: "SHR.       V3",
+		},
+		{
+			label:               "8xy7 set Vx = Vy - Vx, set VF = NOT borrow",
+			opcode:              0x83B7,
+			expectedInstruction: "SUBB.      V3,VB",
+		},
+		{
+			label:               "8xyE set Vx = Vx SHL 1",
+			opcode:              0x83BE,
+			expectedInstruction: "SHL.       V3",
+		},
+		{
+			label:               "9xy0 skip next instruction if Vx != Vy",
+			opcode:              0x93B0,
+			expectedInstruction: "SKIP.NE    V3,VB",
+		},
+		{
+			label:               "Annn set I = nnn",
 			opcode:              0xA220,
-			expectedName:        "MVI",
-			expectedInstruction: "I,#$220",
+			expectedInstruction: "MVI        I,#$220",
+		},
+		{
+			label:               "Bnnn jump to location nnn + V0",
+			opcode:              0xB290,
+			expectedInstruction: "JUMP       $290(V0)",
+		},
+		{
+			label:               "Cxkk set Vx = random byte AND kk",
+			opcode:              0xC9F2,
+			expectedInstruction: "RND        V9,#$F2",
+		},
+		{
+			label:               "Dxyn display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision",
+			opcode:              0xD229,
+			expectedInstruction: "SPRITE.    V2,V2,#$9",
+		},
+		{
+			label:               "Ex9E skip next instruction if key with the value of Vx is pressed",
+			opcode:              0xE79E,
+			expectedInstruction: "SKIP.KEY   V7",
+		},
+		{
+			label:               "ExA1 skip next instruction if key with the value of Vx is not pressed",
+			opcode:              0xE7A1,
+			expectedInstruction: "SKIP.NOKEY V7",
+		},
+		{
+			label:               "Fx07 set Vx = delay timer value",
+			opcode:              0xF907,
+			expectedInstruction: "MOV        V9,DELAY",
+		},
+		{
+			label:               "Fx0A wait for a key press, store the value of the key in Vx",
+			opcode:              0xFC0A,
+			expectedInstruction: "WAITKEY    VC",
+		},
+		{
+			label:               "Fx15 set delay timer = Vx",
+			opcode:              0xF215,
+			expectedInstruction: "MOV        DELAY,V2",
+		},
+		{
+			label:               "Fx18 set sound timer = Vx",
+			opcode:              0xF318,
+			expectedInstruction: "MOV        SOUND,V3",
+		},
+		{
+			label:               "Fx1E set I = I + Vx",
+			opcode:              0xF81E,
+			expectedInstruction: "ADD        I,V8",
+		},
+		{
+			label:               "Fx29 set I = location of sprite for digit Vx",
+			opcode:              0xFD29,
+			expectedInstruction: "SPRITECHAR VD",
+		},
+		{
+			label:               "Fx33 store BCD representation of Vx in memory locations I, I+1, and I+2",
+			opcode:              0xF533,
+			expectedInstruction: "MOVBCD     V5",
+		},
+		{
+			label:               "Fx55 store registers V0 through Vx in memory starting at location I",
+			opcode:              0xF755,
+			expectedInstruction: "MOVM       (I),V0-V7",
+		},
+		{
+			label:               "Fx65 read registers V0 through Vx from memory starting at location I",
+			opcode:              0xFD65,
+			expectedInstruction: "MOVM       V0-VD,(I)",
 		},
 	}
 	for _, c := range cases {
 		t.Run(c.label, func(t *testing.T) {
-			name, instruction, err := c.opcode.Disassemble()
-			require.NoError(t, err, "received 0x%X", c.opcode)
+			instruction, err := c.opcode.Instruction()
+			require.NoError(t, err, "received 0x%X", c.opcode, "opcode not recognized")
 
-			assert.Equal(t, c.expectedName, name)
-			assert.Equal(t, c.expectedInstruction, instruction)
+			assert.Equal(t, c.expectedInstruction, instruction, "incorrect instruction returned for opcode")
 		})
 	}
 }
 
-func TestOpcode_DisassembleUnknownOpcode(t *testing.T) {
-	unknownOpcode := disassembler.Opcode(0x0000)
-	_, _, err := unknownOpcode.Disassemble()
-	require.Error(t, err)
-	require.Equal(t, err, disassembler.ErrUnknownOp)
+func TestOpcode_DisassembleUnknownOpcodes(t *testing.T) {
+	type testCase struct {
+		label  string
+		opcode disassembler.Opcode
+	}
+	cases := []testCase{
+		{
+			label:  "unknown 0 code",
+			opcode: 0x0000,
+		},
+		{
+			label:  "unknown 8 code",
+			opcode: 0x800f,
+		},
+		{
+			label:  "unknown e code",
+			opcode: 0xe000,
+		},
+		{
+			label:  "unknown f code",
+			opcode: 0x0000,
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.label, func(t *testing.T) {
+			_, err := c.opcode.Instruction()
+			require.Error(t, err, "should have received an error")
+			require.Equal(t, err, disassembler.ErrUnknownOp, "error is not the correct type")
+		})
+	}
 }
